41ff34c6d7b643155691acbf5218691b7bf2eb52
SillyFish <kossokx@gmail.com>
2023-07-05 10:47:17 +0800

    add: FontAwesome

--- ShopMall/src/main.ts
+++ ShopMall/src/main.ts
@@ -3,11 +3,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/main.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStore, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faStore, faMagnifyingGlass);
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
+
